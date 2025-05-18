21c607ea5006509c91a5d8ecb969477dcf3b854c
TheShadow1001 <theshadowkiller1234@gmail.com>
2025-05-18 05:33:32 +0200

    Update AppCherry.py

--- AppCherry.py
+++ AppCherry.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import webbrowser
 
 st.info("Bienvenido a App Cherry, Espero te guste:)")
 
@@ -9,7 +10,7 @@ st.write("Con esta plataforma tendrás mucha comodidad a la hora de buscar o ven
 st.write("Cabe aclarar que esta página fue hecho por @WikiDev, talvez no sea alguien conocido.")
 st.write("Esta página es una beta v1.0, así que es posible la precencia de algún error.")
 st.write("Si tiene algúna duda o simplemente quieres dar algún error ocurrido puedes contactarme, por @WikiDeveloper@gmail.com")
-st.subheader("¡Agradezco! su paciencia, Estaré al tanto del proyeyecto lo mejor pósible por su comodidad.")
+st.subheader("¡Agradezco! su paciencia, Estaré al tanto del proyecto lo mejor pósible por su comodidad.")
 
 st.text_input("¿Qué Opinás Del Proyecto?")
 st.button("Envíar Opinión")
@@ -32,3 +33,23 @@ if registrar:
         st.success("¡Registro exitoso!")
     else:
         st.warning("Por favor, completa todos los campos.")
+
+st.subheader("Descripción")
+
+st.write("Esta página fué hecha 100% con streamlit, y la página está siendo mantenida y actualizada por mí.")
+st.write("La página Está en desarrollo, así que ahora no se podrá públicar ni comprar nada, pero dentro de 2 días será posible.")
+st.write("La página Es (BETA), así que es común algún error.")
+
+st.text_input("¿Cómo Debería Mejorarla?")
+
+Acepto = st.button("Acepto")
+
+if Acepto:
+    if Acepto:
+        st.success("Gracias por su opinión:)")
+    else:
+        st.warning("Diga algúna opinión sobre como mejorar la página.")
+
+st.info("Puedes visitar Mi página de juegos:)")
+
+st.write("https://wikiproyects.blogspot.com/2025/05/blog-post.html")
