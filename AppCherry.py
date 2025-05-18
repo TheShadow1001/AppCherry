e0c1033e78fb52ce5903343939b50394edbff3b3
TheShadow1001 <theshadowkiller1234@gmail.com>
2025-05-18 22:07:11 +0200

    Update AppCherry.py

--- AppCherry.py
+++ AppCherry.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import tkinter as tk
+from tkinter import messagebox
 
-st.title("         AppCherry")
+messagebox.showinfo("Welcome to project")
+st.title("AppCherry")
 st.subheader("Welcome To AppCherry")
-st.write("AppCherry es una plataforma web, dónde los vendedores y compradores puedes elegir como una opción elegíble.")
-st.write("Con esta plataforma tendrás mucha comodidad a la hora de buscar o vender productots.")
+st.write("AppCherry es una plataforma web, dónde los vendedores y compradores pueden comprar, vender Etc.")
+st.write("Con esta plataforma tendrás mucha comodidad a la hora de buscar o vender productos.")
 st.write("Cabe aclarar que esta página fue hecho por @WikiDev, talvez no sea alguien conocido.")
 st.write("Esta página es una beta v1.0, así que es posible la precencia de algún error.")
 st.write("Si tiene algúna duda o simplemente quieres dar algún error ocurrido puedes contactarme, por @WikiDeveloper@gmail.com")
@@ -25,4 +28,4 @@ correo = st.text_input("Corréo eléctronico")
 contrasena = st.text_input("contraseña")
 registrar = st.button("Registrarme a AppCherry")
 
-st.subheader("© @WikiDev")
+st.subheader("@WikiDev")
